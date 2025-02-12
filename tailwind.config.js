a709df1bd373a7ee8e6b8f228ff1ad32c5b8678b
payboostbills <sabafarhan10@gmail.com>
2025-02-13 03:54:40 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1C5290',
+        secondary: '#F78F1E',
+      },
+    },
+  },
+  plugins: [],
+};
